cf6faf27e8501b1fccb00c49b7193d09523489f4
KudrMar <130023837+KudrMar@users.noreply.github.com>
2024-05-04 00:37:31 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import '../css/style.css';
+import GameWidget from './game-widget.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const widget = new GameWidget(document.querySelector('.game-widget'), 4);
